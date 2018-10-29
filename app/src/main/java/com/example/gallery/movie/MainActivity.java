940a727119f0029b3fac46488157543a46b8ec47
ririn0062 <ririn.listiana@students.amikom.ac.id>
2018-10-29 15:54:54 +0700

    i upload this file

---
+++ app/src/main/java/com/example/gallery/movie/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.gallery.movie;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<ArrayList<Movie>> {
+    private RecyclerView recyclerView;
+    private ArrayList<Movie> list;
+
+    private String URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=4fdf9ac07bf061eb507fd2ff5033a3ff&region=IN";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.mainRecycler);
+        recyclerView.setHasFixedSize(true);
+        getSupportLoaderManager().initLoader(0, null, (LoaderManager.LoaderCallbacks<ArrayList<Movie>>)this).forceLoad();
+
+    }
+    private void showRecyclerCardView(){
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        CardViewMovieAdapter CardViewMovieAdapter = new CardViewMovieAdapter(this);
+        CardViewMovieAdapter.setMovies(list);
+        recyclerView.setAdapter(CardViewMovieAdapter);
+    }
+
+    @NonNull
+    @Override
+    public Loader<ArrayList<Movie>> onCreateLoader(int i, @Nullable Bundle bundle) {
+        return new MovieDataAsync(this, URL);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<ArrayList<Movie>> loader, ArrayList<Movie> movies) {
+        this.list = movies;
+        Log.d("LIST : ", String.valueOf(this.list.size()));
+        if(this.list != null){
+            showRecyclerCardView();
+        }else{
+            setContentView(R.layout.activity_detail_movie);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<ArrayList<Movie>> loader) {
+        this.list = null;
+
+    }
+}
